188765bdc5bd07370b9aa49360c68be399ec6b86
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-12-04 12:41:17 +0700

    new acc file

---
+++ src/main/java/com/budgettracker/webservices/model/GetAddAccountRequest.java
@@ -0,0 +1,31 @@
+package com.budgettracker.webservices.model;
+
+public class AccountRequest {
+    private String userId;
+    private String name;
+    private String type;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
